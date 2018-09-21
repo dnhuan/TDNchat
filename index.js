a1b19c92af80f2ea580e3ddec4d1450bbb3743dd
dnhuan <huandngoc@gmail.com>
2018-09-21 13:26:31 +0700

    Init MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 var express = require('express')
 var app = express()
-var bodyparser = require('body-parser');
+var bodyparser = require('body-parser')
 var handler = require('./src/handler.js')
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient
+global.db
+
+MongoClient.connect("mongodb://localhost:27017/",{ useNewUrlParser: true }, function(err, db) {
+  if(err) throw err
+  else global.db = db.db("TDNchat")
+})
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
@@ -12,10 +20,13 @@ app.get('/', function (req, res) {
 })
 
 app.post('/TDNchat', function (req, res) {
-    //res.send(200)
-    console.log("ok!")
+    res.sendStatus(200)
+    console.log("Receive request")
     console.log(req.body)
-    handler.handle(req.body.name, req.body.senderID, req.body.msg, req.body.gender)
+    if(req.body.handle_token == "02sr4KvZJ4VECgsMTerD4eMJpUlJ3DVa"){
+      console.log("Token: true")
+      handler.handle(req.body.name, req.body.senderID.toString(), req.body.msg, req.body.gender)
+    }
 })
 
 app.listen(80)
--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,53 @@
+var qu = []
+
 function handle(name, senderID, msg, gender){
-    
+    console.log("Start handler",name," ",senderID)
+    //
+    var user = global.db.collection("User")
+    var query  = user.findOne({'_id': senderID})
+    if(query == undefined){
+    console.log(senderID)
+        user.insertOne({
+            "_id":  senderID,
+            "name": name,
+            "gender": gender,
+            "status": 0,
+            "connect": 0,
+            "chatID": 0
+        })
+    }
+    //status 0-free, 1-pending, 2-paired
+    // checkQueue(senderID,(err,res)=>{
+    //     if(err) console.error(err)
+    //     if(res.status == "Pairing"){
+    //         console.log(res.usrA," <--> ",res.usrB)
+    //     }
+    //     if(res.status == "Queuing"){
+    //         if(res.size == 1) console.log("Queuing: ",res.inline)
+    //     }
+    // })
+}
+
+function checkQueue(senderID,cb){
+    console.log("Checking queue")
+    qu.push(senderID)
+    if(qu.length > 1){
+        var usrA = qu[0], usrB = qu[1];
+        qu.shift()
+        qu.shift()
+        //pairing
+        cb("",{
+            "status": "Pairing",
+            "usrA": usrA,
+            "usrB": usrB
+        })
+    }else{
+        cb("",{
+            "status": "Queuing",
+            "size": qu.length,
+            "inline": qu[0],
+        })
+    }
 }
 
 module.exports = {
